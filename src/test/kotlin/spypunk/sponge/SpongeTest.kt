801d43661b65be6a3a49f42f58ac5e5737581b11
spypunk <spypunk@gmail.com>
2019-12-07 14:29:23 -0800

    Dummy unit test added

---
+++ src/test/kotlin/spypunk/sponge/SpongeTest.kt
@@ -0,0 +1,20 @@
+/**
+ * Copyright © 2019 spypunk <spypunk@gmail.com>
+ *
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
+package spypunk.sponge
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class SpongeTest {
+
+    @Test
+    fun dummyTest() {
+        Assertions.assertTrue(true)
+    }
+}
