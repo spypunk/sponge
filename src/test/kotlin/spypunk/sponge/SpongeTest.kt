305a9582c11f2e834d19bd0db4efe63946314899
spypunk <spypunk@gmail.com>
2019-12-08 23:34:00 -0800

    Unit tests updated

--- src/test/kotlin/spypunk/sponge/SpongeTest.kt
+++ src/test/kotlin/spypunk/sponge/SpongeTest.kt
@@ -11,27 +11,27 @@ package spypunk.sponge
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.apache.http.entity.ContentType
 import org.jsoup.Connection
-import org.jsoup.helper.DataUtil
+import org.jsoup.Jsoup
 import org.junit.jupiter.api.Test
-import java.io.ByteArrayInputStream
 import java.io.File
 import java.net.URI
-import java.nio.charset.StandardCharsets
 
 class SpongeTest {
-    private val spongeService = mockk<SpongeService>()
+    private val spongeService = mockk<SpongeService>(relaxed = true)
+    private val spongeInput = SpongeInput(
+            URI("https://www.test.com"),
+            File("output"),
+            setOf(ContentType.TEXT_PLAIN.mimeType)
+    )
 
     @Test
-    fun testDocumentWithoutLinks() {
-        val spongeInput = SpongeInput(
-                URI("https://www.test.com"),
-                File("output"),
-                setOf("text/plain")
-        )
+    fun testEmptyDocument() {
+        val htmlContent = "<html></html>"
 
         every { spongeService.connect(spongeInput.uri) } returns
-                response("text/html", "<html></html>", spongeInput.uri)
+                response(htmlContent, spongeInput.uri)
 
         executeSponge(spongeInput)
 
@@ -39,17 +39,47 @@ class SpongeTest {
         verify(exactly = 0) { spongeService.download(any(), any()) }
     }
 
-    private fun response(contentType: String, content: String, uri: URI): Connection.Response {
+    @Test
+    fun testDocumentWithTextLink() {
+        val fileName = "test.txt"
+
+        val htmlContent =
+                """
+                    <html>
+                        <body>
+                            <a href="/$fileName" />
+                        </body>
+                    </html>
+                """
+
+        every { spongeService.connect(spongeInput.uri) } returns
+                response(htmlContent, spongeInput.uri)
+
+        val fileUri = URI("${spongeInput.uri}/$fileName")
+
+        every { spongeService.connect(fileUri) } returns
+                response(ContentType.TEXT_PLAIN.mimeType)
+
+        executeSponge(spongeInput)
+
+        verify(exactly = 1) { spongeService.connect(spongeInput.uri) }
+        verify(exactly = 1) { spongeService.connect(fileUri) }
+        verify(exactly = 1) { spongeService.download(fileUri, File(spongeInput.outputDirectory, fileName)) }
+    }
+
+    private fun response(htmlContent: String, baseUri: URI): Connection.Response {
+        val response = mockk<Connection.Response>()
+
+        every { response.contentType() } returns ContentType.TEXT_HTML.mimeType
+        every { response.parse() } returns Jsoup.parse(htmlContent, baseUri.toString())
+
+        return response
+    }
+
+    private fun response(contentType: String): Connection.Response {
         val response = mockk<Connection.Response>()
 
         every { response.contentType() } returns contentType
-
-        every { response.parse() } returns
-                DataUtil.load(
-                        ByteArrayInputStream(content.toByteArray(StandardCharsets.UTF_8)),
-                        StandardCharsets.UTF_8.name(),
-                        uri.toString()
-                )
 
         return response
     }
